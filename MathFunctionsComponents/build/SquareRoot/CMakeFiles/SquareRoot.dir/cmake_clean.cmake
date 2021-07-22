9e51384b6b707ef7901f43733ab3e11a611dd2f9
Madhav Achar <machar@quantumsignalai.com>
2021-07-22 15:10:21 -0400

    Finished components exercise

---
+++ MathFunctionsComponents/build/SquareRoot/CMakeFiles/SquareRoot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SquareRoot.dir/SquareRoot.cxx.o"
+  "libSquareRoot.a"
+  "libSquareRoot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SquareRoot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
